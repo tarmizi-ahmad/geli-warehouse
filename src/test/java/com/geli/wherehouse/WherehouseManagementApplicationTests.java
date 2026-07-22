96b2b122c8bdcddbf5910f8c5db032f5b146bf48
tarmizi-ahmad <ahmad.tarmizi.albed@hotmail.com>
2026-07-22 16:58:30 +0700

    Create project package structure

---
+++ src/test/java/com/geli/wherehouse/WherehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geli.wherehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WherehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
